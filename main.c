bdcef6ca3525e53bab234464c398f51932c51021
Tom Barbette <tom.barbette@ulg.ac.be>
2018-08-30 15:07:15 +0200

    Use a doubler for this example

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, int argv) {
     printf("Hello world !");
+
+    if (argc <= 1)
+        return 1;
+
+    int n;
+    scanf("%d", &n);
+
+    printf("%d", n * 2);
+
     return 0;
 }
